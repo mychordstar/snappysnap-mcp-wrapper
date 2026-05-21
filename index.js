d3ab9911f5a64a9d10d07dbd91a7c08b1dd41d5c
mychordstar <mychordstar@gmail.com>
2026-05-21 12:59:04 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const { spawn } = require('child_process');
+
+const server = spawn('C:\\Program Files\\SnappySnap\\SnappyMCPServer.exe', ['--stdio'], {
+  stdio: ['pipe', 'pipe', 'pipe']
+});
+
+process.stdin.pipe(server.stdin);
+server.stdout.pipe(process.stdout);
+server.stderr.pipe(process.stderr);
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+  process.exit(1);
+});
